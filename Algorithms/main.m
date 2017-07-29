d100e37e4244f1aab0db1c09e68c0dfb0f54b0f6
Marianne Glad <marianneglad@mariannes-mbp>
2017-07-29 13:43:15 +0200

    Initial Commit

---
+++ Algorithms/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Algorithms
+//
+//  Created by Marianne Glad on 7/29/17.
+//  Copyright © 2017 slingthor. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
